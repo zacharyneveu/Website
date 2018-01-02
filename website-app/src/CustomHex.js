2a8d679e8fec36e1bf49eefd716d4a040f383235
Neveu, Zachary [DPUYS] <zneveu@its.jnj.com>
2018-01-02 15:21:08 -0500

    Wrap hexagons for easier use

---
+++ website-app/src/CustomHex.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Hexagon, Text } from 'react-hexgrid';
+import PropTypes from 'prop-types';
+
+class CustomHex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.hexEntered = this.hexEntered.bind(this);
+        this.hexLeft = this.hexLeft.bind(this);
+        this.goToPage = this.goToPage.bind(this);
+        this.state = {hovered: false};
+    }
+
+    /** When mouse enters hexagon */
+    hexEntered() {
+        this.setState({ hovered: true });
+    }
+
+    /** When mouse leaves hexagon */
+    hexLeft() {
+        this.setState({ hovered: false });
+    }
+
+    /** Navigate to a route passed in as a prop */
+    goToPage() {
+        if(this.props.route) {
+            window.location.replace(this.props.route);
+        }
+    }
+
+    render() {
+        return (
+            <Hexagon onMouseEnter={this.hexEntered}
+                onMouseLeave={this.hexLeft}
+                fill={this.props.fill ? this.props.fill : null}
+                onClick={this.goToPage}
+                q={this.props.q}
+                r={this.props.r}
+                s={this.props.s}
+                >
+                <Text>{this.state.hovered ? this.props.text : ""}</Text>
+            </Hexagon>
+        )
+    }
+}
+
+CustomHex.PropTypes = {
+    /**URL with image to fill hexagon */
+    fill: PropTypes.string,
+    /** text to display in hexagon */
+    text: PropTypes.string,
+    /** Route to navigate to on click */
+    route: PropTypes.string,
+    /** coordinates of hexagon in grid */
+    q: PropTypes.number.isRequired,
+    r: PropTypes.number.isRequired,
+    s: PropTypes.number.isRequired,
+}
+
+export default CustomHex;
--- website-app/src/HomePage.js
+++ website-app/src/HomePage.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { HexGrid, Layout, Hexagon, Text, Pattern} from 'react-hexgrid';
+import { HexGrid, Layout, Hexagon, Text, Pattern } from 'react-hexgrid';
 //import colors from './Colors'
+import CustomHex from './CustomHex';
 
 class HomePage extends React.Component {
   constructor(props) {
-      super(props);
-      this.toAbout = this.toAbout.bind(this);
+    super(props);
+    this.toAbout = this.toAbout.bind(this);
+    this.hexEntered = this.hexEntered.bind(this);
+    this.hexLeft = this.hexLeft.bind(this);
+    this.state = { celloText: "" }
   }
 
+  /** Navigate to the about screen */
   toAbout() {
-      window.location.replace('/about');
+    window.location.replace('/about');
   }
+
+  /** When a hexagon is entered, update its text */
+  hexEntered() {
+    this.setState({ celloText: "Electric Cello" });
+  }
+
+  /** when a hexagon is exited, return text to blank */
+  hexLeft() {
+    this.setState({ celloText: "" });
+  }
+
   render() {
 
     const layoutSize = {
@@ -31,11 +47,11 @@ class HomePage extends React.Component {
         <HexGrid width='auto' height='100vh' viewBox="-50 -50 100 100">
           <Layout size={layoutSize} flat={true} spacing={1.1} origin={layoutOG}>
             {/*About Page*/}
-            <Hexagon q={0} r={0} s={0}i onClick={this.toAbout}>
-               <Text>ZN</Text>
+            <Hexagon q={0} r={0} s={0} i onClick={this.toAbout}>
+              <Text>ZN</Text>
             </Hexagon>
             <Hexagon q={0} r={-1} s={1}>
-               <Text>Code Samples</Text>
+              <Text>Code Samples</Text>
             </Hexagon>
             <Hexagon q={0} r={1} s={-1}>
               <Text>Resume</Text>
@@ -48,24 +64,36 @@ class HomePage extends React.Component {
               <Text>Writing Sample</Text>
             </Hexagon>
 
-            <Hexagon q={-1} r={1} s={0} fill="linkedin">
-            </Hexagon>
-            <Hexagon q={1} r={-1} s={0}>
-            </Hexagon>
+            <CustomHex q={-1} r={1} s={0} fill="cello" 
+                  text="Electric Cello"
+                  route="/about" />
+
+            <CustomHex q={1} r={-1} s={0} fill="alexa"
+                  text="Alexa Speaker"
+                  route="/alexa"/>
           </Layout>
 
-          <Pattern id="fb" 
-                   link="http://zacharyneveu.com/Images/facebook.png" 
-                   size={iconSize} />
+          <Pattern id="fb"
+            link="http://zacharyneveu.com/Images/facebook.png"
+            size={iconSize} />
 
-          <Pattern id="linkedin" 
-                   link= "http://zacharyneveu.com/Images/linkedin.png"
-                   size={iconSize} />
+          <Pattern id="linkedin"
+            link="http://zacharyneveu.com/Images/linkedin.png"
+            size={iconSize} />
+
+          {/* Cello Scroll*/}
+          <Pattern id="cello"
+            link="http://zacharyneveu.com/Images/celleft.png"
+            size={layoutSize} />
+
+          <Pattern id="alexa"
+            link="http://zacharyneveu.com/Images/DodecFinal.jpg"
+            size={layoutSize} />
 
         </HexGrid>
       </div>
     );
-}
+  }
 }
 
 export default HomePage;
