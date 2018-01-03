50b2c3b7d55cfdccad4be9325f5731977772cf94
Neveu, Zachary [DPUYS] <zneveu@its.jnj.com>
2018-01-03 00:31:47 -0500

    Add alexa page

---
+++ src/AlexaSpeakerPage.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import LargeHex from './LargeHex';
+import HomeButton from './Buttons'
+import PhotoGallery from './PhotoGallery';
+import './ContentPage.css';
+
+class AlexaSpeakerPage extends React.Component {
+    render() {
+        var imageSrcs = [
+          "http://zacharyneveu.com/Images/DodecFinal.jpg",
+          "http://zacharyneveu.com/Images/DodecInsides.jpg"
+        ];
+        return(
+            <div className="PageContainer">
+                <LargeHex />
+                <div className="Page">
+                    <br />
+                    <h1>Custom Alexa Speaker</h1> 
+                    <br />
+                    <p> 
+                        I found an older model bluetooth speaker at a thrift
+                        store for $10 and bought it with the intention of giving
+                        it new life as a smart home hub, complete with a
+                        personal assistant and a cool new form factor. The
+                        project took about a week and a half of free time, and I
+                        am very happy with the final result. 
+                    </p> 
+                    <h2>Personal Assistant</h2> 
+                    <p> 
+                        To incorporate a personal assistant into the new device,
+                        I used Amazon's Alexa AVS to run an Alexa client on a
+                        Raspberry Pi (Model 2b because that's what I had lying
+                        around). I coupled the Pi with a USB microphone and an
+                        external HiFiBerry DAC in order to get clear audio with
+                        minimal noise when not in use. The audio from the DAC is
+                        then sent through a 25W amplifier, a (very) simple mixer
+                        circuit with the original bluetooth audio, and is passed
+                        to the woofer and tweeter (with a simple high pass
+                        filter on the tweeter). The original bluetooth speaker
+                        was left intact and is a secondary way to play songs
+                        that might not be accessible through Alexa. As an
+                        additional personalization, I used the Snowboy wake work
+                        engine to enable a custom wake word (name) for the
+                        assistant. 
+                    </p> 
+                    <PhotoGallery images={imageSrcs}
+                                  height="10vh"
+                                  width="auto"
+                                  interval={1}/>
+                    <HomeButton />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AlexaSpeakerPage;
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './HomePage'
 import AboutPage from './AboutPage'
+import AlexaSpeakerPage from './AlexaSpeakerPage'
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <div>
         <Route exact path='/' component={HomePage}/>
         <Route path='/about' component={AboutPage}/>
+        <Route path='/alexa' component={AlexaSpeakerPage}/>
       </div>
     )
   }
