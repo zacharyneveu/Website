ae54618e0cc17c365b35fdec480d4eca9010da7d
Neveu, Zachary [DPUYS] <zneveu@its.jnj.com>
2018-01-02 19:39:33 -0500

    Restructure to remove old site

--- old_reference/code_snippets/languages/shiftweb.v
+++
@@ -1,7 +0,0 @@
-module ShiftLeft(input[31:0] in,
-		output[31:0] out);
-
-	assign out = in &lt;&lt; 2;
-
-endmodule
-
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './HomePage'
+import AboutPage from './AboutPage'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/about' component={AboutPage}/>
+      </div>
+    )
+  }
+}
+
+export default App;
