d673de27770fa6fae240f326c543a039b0a9c492
Zachary Neveu <zachary.neveu@gmail.com>
2018-01-07 19:48:29 -0500

    Don't render hex if height>width of page

--- src/LargeHex.js
+++ src/LargeHex.js
@@ -2,13 +2,36 @@ import React from 'react';
 import { Hexagon, HexGrid, Layout } from 'react-hexgrid';
 
 
-/** LargeHex creates a full-page hexagon */
+/** LargeHex creates a full-page hexagon 
+ *  LargeHex keeps track of the window dimensions in order to not render
+ * if page is too tall (i.e. on a mobile device)
+ */
 class LargeHex extends React.Component {
+    constructor(props) {
+        super(props);
+        /** Keep track of window dimensions */
+        this.state = {width: 0, height: 0};
+        this.updateDimensions = this.updateDimensions.bind(this);
+    }
+
+    /** update state on window resize */
+    updateDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight});
+    }
+
+    /** Setup component to update state on window resize */
+    componentDidMount() {
+        this.updateDimensions();
+        window.addEventListener('resize', this.updateDimensions);
+    }
+
+    /** Don't update state if component no longer used */
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateDimensions);
+    }
+
     render() {
-        const largeHexSize = {
-            x: 50,
-            y: 50
-        }
+        const largeHexSize = this.state.height < this.state.width ? {x:50, y:50} : {x:0, y:0};
         return (
             <HexGrid width='auto' height='100vh' viewBox="-50 -50 100 100">
                 <Layout size={largeHexSize} 
